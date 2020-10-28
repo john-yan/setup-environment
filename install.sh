63d13933741cf9f202ef7dd8fb72a985b29a8cef
Junliang Yan <jyan@ca.ibm.ca>
2020-10-28 11:27:50 -0400

    Add gitconfig

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ bash ~/.vim_runtime/install_awesome_vimrc.sh
 # install screenrc
 cp screenrc ~/.screenrc
 
+# install gitconfig
+cp gitconfig ~/.gitconfig
+
 # install ssh keys
 cat /dev/zero | ssh-keygen -t rsa -N ""
 
